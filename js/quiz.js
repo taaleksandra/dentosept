f9377c540ee63897815c152e660248b970013b2c
Aleksandra <94486752+taaleksandra@users.noreply.github.com>
2025-02-28 13:36:37 +0100

    quiz icons

--- js/quiz.js
+++ js/quiz.js
@@ -4,51 +4,73 @@ const quizData = [
     options: [
       {
         text: "Codzienna higiena i pielęgnacja",
+        icon: "img/quiz-icons/codzienna-higiena-i-pielegnacja.png",
         next: [
           {
             question: "Wybierz rodzaj pielęgnacji:",
             options: [
               {
                 text: "Pielęgnacja po zakończeniu leczenia",
+                icon: "img/quiz-icons/po-zakonczeniu-leczenia.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
                       {
                         text: "Płyn do płukania",
+                        icon: "img/quiz-icons/plyn-do-plukania.png",
                         product: "Dentosept Complex",
                         age: "6+",
                       },
-                      { text: "Spray", product: "Dentosept Fast", age: "6+" },
+                      {
+                        text: "Spray",
+                        icon: "img/quiz-icons/spray.png",
+                        product: "Dentosept Fast",
+                        age: "6+",
+                      },
                     ],
                   },
                 ],
               },
               {
                 text: "Codzienna higiena jamy ustnej",
+                icon: "img/quiz-icons/codzienna-higiena-i-pielegnacja.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
                       {
                         text: "Płyn do płukania",
+                        icon: "img/quiz-icons/plyn-do-plukania.png",
                         product: "Dentosept Complex",
                         age: "6+",
                       },
-                      { text: "Spray", product: "Dentosept A", age: "12+" },
+                      {
+                        text: "Spray",
+                        icon: "img/quiz-icons/spray.png",
+                        product: "Dentosept A",
+                        age: "12+",
+                      },
                     ],
                   },
                 ],
               },
               {
                 text: "Pielęgnacja błony śluzowej",
+                icon: "img/quiz-icons/pielegnacja-blony-sluzowej.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
-                      { text: "Spray", product: "Dentosept Fast", age: "6+" },
+                      {
+                        text: "Spray",
+                        icon: "img/quiz-icons/spray.png",
+                        product: "Dentosept Fast",
+                        age: "6+",
+                      },
                       {
                         text: "Płyn do płukania",
+                        icon: "img/quiz-icons/plyn-do-plukania.png",
                         product: "Dentosept Complex",
                         age: "6+",
                       },
@@ -58,13 +80,20 @@ const quizData = [
               },
               {
                 text: "Pielęgnacja podczas noszenia aparatów ortodontycznych",
+                icon: "img/quiz-icons/aparaty-ort.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
-                      { text: "Spray", product: "Dentosept A", age: "12+" },
+                      {
+                        text: "Spray",
+                        icon: "img/quiz-icons/spray.png",
+                        product: "Dentosept A",
+                        age: "12+",
+                      },
                       {
                         text: "Płyn do płukania",
+                        icon: "img/quiz-icons/plyn-do-plukania.png",
                         product: "Dentosept Complex",
                         age: "6+",
                       },
@@ -78,39 +107,50 @@ const quizData = [
       },
       {
         text: "Profilaktyka i pielęgnacja",
+        icon: "img/quiz-icons/profilaktyka-i-pielegnacja.png",
         next: [
           {
             question: "Wybierz rodzaj profilaktyki:",
             options: [
               {
                 text: "Próchnica",
+                icon: "img/quiz-icons/prochnica.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
                       {
                         text: "Płyn do płukania",
+                        icon: "img/quiz-icons/plyn-do-plukania.png",
                         product: "Dentosept Complex",
                         age: "6+",
                       },
-                      { text: "Spray", product: "Dentosept Fast", age: "6+" },
+                      {
+                        text: "Spray",
+                        icon: "img/quiz-icons/spray.png",
+                        product: "Dentosept Fast",
+                        age: "6+",
+                      },
                     ],
                   },
                 ],
               },
               {
                 text: "Wzmocnienie szkliwa",
+                icon: "img/quiz-icons/wzmocnienie-szkliwa.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
                       {
                         text: "Spray",
+                        icon: "img/quiz-icons/spray.png",
                         product: "Dentosept Probiodent KID",
                         age: "3+",
                       },
                       {
                         text: "Płyn do płukania",
+                        icon: "img/quiz-icons/plyn-do-plukania.png",
                         product: "Dentosept Complex",
                         age: "6+",
                       },
@@ -124,69 +164,107 @@ const quizData = [
       },
       {
         text: "Leczenie i pielęgnacja",
+        icon: "img/quiz-icons/leczenie-i-pielegnacja.png",
         next: [
           {
             question: "Wybierz problem leczniczy:",
             options: [
               {
                 text: "Dziąsła",
+                icon: "img/quiz-icons/dziasla.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
-                      { text: "Żel", product: "Dentosept Pen", age: "2+" },
-                      { text: "Spray", product: "Dentosept Fast", age: "6+" },
+                      {
+                        text: "Żel",
+                        icon: "img/quiz-icons/gel.png",
+                        product: "Dentosept Pen",
+                        age: "2+",
+                      },
+                      {
+                        text: "Spray",
+                        icon: "img/quiz-icons/spray.png",
+                        product: "Dentosept Fast",
+                        age: "6+",
+                      },
                     ],
                   },
                 ],
               },
               {
                 text: "Jama ustna",
+                icon: "img/quiz-icons/jama ustna.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
                       {
                         text: "Płyn do płukania",
+                        icon: "img/quiz-icons/plyn-do-plukania.png",
                         product: "Dentosept 100",
                         age: "6+",
                       },
-                      { text: "Spray", product: "Dentosept Fast", age: "6+" },
+                      {
+                        text: "Spray",
+                        icon: "img/quiz-icons/spray.png",
+                        product: "Dentosept Fast",
+                        age: "6+",
+                      },
                     ],
                   },
                 ],
               },
               {
                 text: "Afty",
+                icon: "img/quiz-icons/afty.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
-                      { text: "Żel", product: "Dentosept Pen", age: "2+" },
-                      { text: "Spray", product: "Dentosept Fast", age: "6+" },
+                      {
+                        text: "Żel",
+                        icon: "img/quiz-icons/gel.png",
+                        product: "Dentosept Pen",
+                        age: "2+",
+                      },
+                      {
+                        text: "Spray",
+                        icon: "img/quiz-icons/spray.png",
+                        product: "Dentosept Fast",
+                        age: "6+",
+                      },
                     ],
                   },
                 ],
               },
               {
                 text: "Odleżyny protetyczne",
+                icon: "img/quiz-icons/olezyny-prot.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
-                      { text: "Spray", product: "Dentosept A", age: "12+" },
+                      {
+                        text: "Spray",
+                        icon: "img/quiz-icons/spray.png",
+                        product: "Dentosept A",
+                        age: "12+",
+                      },
                     ],
                   },
                 ],
               },
               {
                 text: "Paradontoza",
+                icon: "img/quiz-icons/paradontoza.png",
                 next: [
                   {
                     question: "Wybierz typ aplikatora:",
                     options: [
                       {
                         text: "Płyn do płukania",
+                        icon: "img/quiz-icons/plyn-do-plukania.png",
                         product: "Dentosept 100",
                         age: "6+",
                       },
@@ -226,7 +304,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     stepData.options.forEach((option) => {
       const button = document.createElement("button");
-      button.innerText = option.text;
+      button.className = "quiz-option";
+
+      // Tworzenie obrazka dla ikony
+      if (option.icon) {
+        const icon = document.createElement("img");
+        icon.src = option.icon;
+        icon.alt = option.text;
+        icon.className = "quiz-icon"; // Stylizacja ikonki
+        button.appendChild(icon);
+      }
+
+      // Tekst opcji
+      const textSpan = document.createElement("span");
+      textSpan.innerText = option.text;
+      button.appendChild(textSpan);
+
       button.addEventListener("click", () => {
         if (option.next) {
           showStep(option.next[0]);
